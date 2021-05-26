dd46d099aa617a59b0ec5dae845d70610f97726a
wz2cool <wz2cool@live.cn>
2021-05-26 14:25:29 +0800

    init

---
+++ src/main/java/com/github/wz2cool/rocketmqsync/RocketmqSyncApplication.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.rocketmqsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wz2cool/rocketmqsync/RocketmqSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.rocketmqsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
